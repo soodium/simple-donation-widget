1add63aa3985675154c76ca806437fe662ff5d4d
Vinit Sood <vinit.sood@netlight.com>
2017-02-28 12:37:30 +0100

    remove console.logs and routing script in index

--- app/donation-widget/donation-widget.js
+++ app/donation-widget/donation-widget.js
@@ -6,36 +6,35 @@ var widget = angular.module('myApp.donation-widget', [])
     .directive('donationWidget', function() {
         return {
             templateUrl: './donation-widget/donation-widget.html',
-            controller: 'donationWidgetController' ,
+            controller: 'donationWidgetController',
             controllerAs: 'donationWidgetVm',
             restrict: 'E',
         };
 
-    }).controller( 'donationWidgetController',function() {
-      
-    // Projects information
-    console.log(this);
-    this.project = {
-        donationGoal: 500,
-        progressProcent: 0,
-        totalDonations: 0,
-        numberOfDonors: 0,
-        endDate: 0,
-        isGoalReached: false
-    }
+    }).controller('donationWidgetController', function() {
 
-    // function for 'donating' to the project
-    this.giveNow = function() {
-        if (typeof this.donation === 'number') {
-            // update project status
-            this.project.progressProcent += this.donation / 5.0;
-            this.project.totalDonations += this.donation;
-            this.project.numberOfDonors += 1;
+        // Projects information
+        this.project = {
+            donationGoal: 500,
+            progressProcent: 0,
+            totalDonations: 0,
+            numberOfDonors: 0,
+            endDate: 0,
+            isGoalReached: false
+        }
 
-            // check if goal is reached
-            if (this.project.totalDonations >= this.project.donationGoal) {
-                this.project.isGoalReached = true;
+        // function for 'donating' to the project
+        this.giveNow = function() {
+            if (typeof this.donation === 'number') {
+                // update project status
+                this.project.progressProcent += this.donation / 5.0;
+                this.project.totalDonations += this.donation;
+                this.project.numberOfDonors += 1;
+
+                // check if goal is reached
+                if (this.project.totalDonations >= this.project.donationGoal) {
+                    this.project.isGoalReached = true;
+                }
             }
         }
-    }
-});
+    });
--- app/donation-widget/donation-widget_test.js
+++ app/donation-widget/donation-widget_test.js
@@ -2,28 +2,28 @@
 
 
 describe('donationWidgetController', function() {
-beforeEach(module('myApp.donation-widget'));
+    beforeEach(module('myApp.donation-widget'));
 
-var $controller;
+    var $controller;
 
-beforeEach(inject(function(_$controller_) {
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $controller = _$controller_;
+    beforeEach(inject(function(_$controller_) {
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
 
-}));
+    }));
 
-describe('$scope.project', function() {
-    it('updates goal rechaed status when donation goal is reached', function() {
-        var $scope = {};
-        var controller = $controller('donationWidgetController', {
-            $scope: $scope
+    describe('donationWidgetController', function() {
+        it('updates goal reached status when donation goal is met', function() {
+            var $scope = {};
+            var controller = $controller('donationWidgetController', {
+                $scope: $scope
+            });
+
+            controller.donation = 501;
+            controller.giveNow();
+            expect(controller.project.donationGoal).toEqual(500);
+            expect(controller.project.isGoalReached).toEqual(true);
+
         });
-        
-        controller.donation = 501;
-        controller.giveNow();
-        expect(controller.project.donationGoal).toEqual(500);
-        expect(controller.project.isGoalReached).toEqual(true);
-
     });
-});
 });
